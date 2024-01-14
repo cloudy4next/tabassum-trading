0a2a5796b87c28a5e1819e8589ed2f4560db10a2
Your Name <jahangir720@live.com>
2024-01-15 01:43:21 +0600

    chore: documentation

--- cloudy4next/src/app/Contracts/Cloudy4nextInterface.php
+++ cloudy4next/src/app/Contracts/Cloudy4nextInterface.php
@@ -12,5 +12,11 @@ interface Cloudy4nextInterface
     function createOperation();
 
     function initEdit($id, $actionRoute, $componentData = null);
+
+    /**
+     * @param mixed $id to find coresponding data of item.
+     *
+     * @return array should like this [componentName => componentData,etc...]
+     */
     function setComponentData($id);
 }
